373db060139f888aea94a3743d99c7aea4e5c5c1
Per Lundkvist <persyl@gmail.com>
2016-03-10 20:18:16 +0100

    First edit on startpage

--- index.js
+++ index.js
@@ -11,15 +11,18 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i = 0; i < times; i++)
+        result += cool();
+    response.send(result);
+    //response.render('pages/index');
 });
 
 app.get('/cool', function(request, response) {
-  response.send(cool());
+    response.send(cool());
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
